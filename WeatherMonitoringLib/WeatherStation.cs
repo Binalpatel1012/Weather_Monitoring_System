4c5cd2c9f15c53886d370955af9725eb77b7314e
Binal Patel <147269633+Binalpatel1012@users.noreply.github.com>
2024-04-08 02:44:48 +0000

    Implement the Factory - WeatherStation

---
+++ WeatherMonitoringLib/WeatherStation.cs
@@ -0,0 +1,22 @@
+using System; 
+
+namespace WeatherMonitoringLib
+{
+    public class WeatherStation
+    {
+        public static IDisplay CreateDisplay(string type)
+        {
+            switch (type.ToLower())
+            {
+                case "currentconditions":
+                    return new CurrentConditionsDisplay(WeatherData.Instance);
+                case "statistics":
+                    //return new StatisticsDisplay(WeatherData.Instance);
+                case "forecast":
+                    //return new ForecastDisplay(WeatherData.Instance);
+                default:
+                    throw new ArgumentException("Unknown display type.");
+            }
+        }
+    }
+}
