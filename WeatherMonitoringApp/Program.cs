80b8b2b00fa345517948e74fd33eae1a15361911
Binal Patel <147269633+Binalpatel1012@users.noreply.github.com>
2024-04-08 02:59:37 +0000

    Made some changes after got the error

--- WeatherMonitoringApp/Program.cs
+++ WeatherMonitoringApp/Program.cs
@@ -10,8 +10,6 @@ class Program
         var statsDisplay = WeatherStation.CreateDisplay("statistics") as IDisplay;
         var forecastDisplay = WeatherStation.CreateDisplay("forecast") as IDisplay;
 
-        // Simulate new weather measurements
+        
         weatherData.SetMeasurements(25.3f, 65f, 1013.1f);
-        weatherData.SetMeasurements(24.7f, 70f, 1012.5f);
-    }
 }
